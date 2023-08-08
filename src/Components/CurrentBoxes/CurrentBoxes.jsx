5e4c350dc2e59db335b7da33fe79f68a59b017c2
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-08 18:47:54 -0400

    Added colors to container boxes

--- src/Components/CurrentBoxes/CurrentBoxes.jsx
+++ src/Components/CurrentBoxes/CurrentBoxes.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export const CurrentBoxes = () => {
     return (
         <div id="currentBoxContainer"> 
-            <div className="currentBox"></div>
-            <div className="currentBox"></div>
-            <div className="currentBox"></div>
+            <div className="currentBox schedule"></div>
+            <div className="currentBox exams"></div>
+            <div className="currentBox assignments"></div>
         </div>
     )
 };
