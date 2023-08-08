2da0207bd97f67a98f7048b04ba4389269075174
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-07 23:55:52 -0400

    Copied code from vanilla js

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { CalendarHeader } from "../CalendarHeader";
+import { Day } from "../Day"
 
 export const App = () => {
 
@@ -26,6 +27,57 @@ export const App = () => {
         localStorage.setItem("events", JSON.stringify(events))
     }, [events]);
 
+    useEffect(() => {
+        const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+        const dt = new Date();
+
+        if (nav !== 0) {
+            dt.setMonth(new Date().getMonth() + nav);
+        }
+
+        const day = dt.getDate();
+        const month = dt.getMonth();
+        const year = dt.getFullYear();
+
+        // (year, the next month, the last day of previous month, so basically giving the last day on the month)
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+        const firstDayOfMonth = new Date(year, month, 1);
+
+        const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
+            weekday: "long",
+            year: "numeric",
+            month: "numeric",
+            day: "numeric"
+        })
+        setDateDisplay(`${dt.toLocaleDateString("en-us", { month: "long" } )} ${year}`);
+        const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
+
+        const daysArr = []
+
+        for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+            const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+            if (i > paddingDays) {
+                daysArr.push({
+                    value: i - paddingDays,
+                    event: eventForDate(dayString),
+                    isCurrentDay: i - paddingDays === day && nav === 0,
+                    date: dayString
+                })
+            } else {
+                daysArr.push({
+                    value: "padding",
+                    event: null,
+                    isCurrentDay: false,
+                    date: ""
+                })
+            }
+        }
+
+        setDays(daysArr);
+
+    }, [events, nav]);
+
     return(
         <div id="container">
             <CalendarHeader />
@@ -40,7 +92,19 @@ export const App = () => {
                 <div>Saturday</div>
             </div>
 
-            <div id="calendar"></div>
+            <div id="calendar">
+                {days.map((d, index) => (
+                    <Day
+                        key={index}
+                        day={d}
+                        onClick={() => {
+                            if (day.value != padding) {
+                                setClicked(day.date);
+                            }
+                        }}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
