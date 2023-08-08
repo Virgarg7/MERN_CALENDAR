e061d82434e2d82917b6ede00f82a79c73f506cd
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-08 00:25:40 -0400

    Rendered days in single month

--- src/Day/Day.jsx
+++ src/Day/Day.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export const Day = () => {
-    return (<>Hello from the Day Component</>)
+export const Day = ({ day, onClick }) => {
+    const className = `day ${day.value === "padding" ? "padding" : ""} ${day.isCurrentDay ? "currentDay" : ""}`;
+    return (
+        <div className={className}>
+            {day.value === "padding" ? "" : day.value}
+            {day.event && <div className="event">{day.event.title}</div>}
+        </div>
+    );
 };
