988bd7b530c4a7947bccca47ea8d98daccf6eb3c
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-08 04:58:29 -0400

    Changed button positions

--- src/CalendarHeader/CalendarHeader.jsx
+++ src/CalendarHeader/CalendarHeader.jsx
@@ -5,9 +5,8 @@ export const CalendarHeader = ({ onNext, onBack, dateDisplay, goToday}) => {
         <div id="header">
             <div id="monthDisplay">{dateDisplay}</div>
             <div>
-                <button onClick={goToday} id="todayButton">Today</button>
-
                 <button onClick={onBack} id="backButton">Back</button>
+                <button onClick={goToday} id="todayButton">Today</button>
                 <button onClick={onNext} id="nextButton">Next</button>
             </div>
         </div>
