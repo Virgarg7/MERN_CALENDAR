b1ed491ae30ea5e02e3365a78aa5a0f5e07becde
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-08 20:45:16 -0400

    Refactored day code

--- src/Components/Day/Day.jsx
+++ src/Components/Day/Day.jsx
@@ -4,7 +4,7 @@ export const Day = ({ day, onClick }) => {
     const className = `day ${day.padding === true ? "padding" : ""} ${day.isToday ? "today" : ""}`;
     return (
         <div onClick={onClick} className={className}>
-            {day.value === "paddingPrev" ? "" : day.value}
+            {day.value}
             {day.event && <div className="event">{day.event.title}</div>}
         </div>
     );
