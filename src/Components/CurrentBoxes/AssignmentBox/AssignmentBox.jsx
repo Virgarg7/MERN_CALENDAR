7198713b7ea55c5dde7b4e89919c3128dbcf468c
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-08-10 04:38:54 -0400

    Started adding UI elements in assignments box

--- src/Components/CurrentBoxes/AssignmentBox/AssignmentBox.jsx
+++ src/Components/CurrentBoxes/AssignmentBox/AssignmentBox.jsx
@@ -3,7 +3,14 @@ import React from "react";
 export const AssignmentBox = ( { onClick }) => {
     return (
         <>
-            <div onClick={onClick} className="currentBox assignments"></div>
+            <div className="currentBox assignments">
+                <div id="AssignmentBoxHeader">
+                    <div id="assignmentDisplay">Assignments:</div>
+                    <button onClick={onClick}>+</button>
+                </div>
+                
+                
+            </div>
         </>
     );
 };
